030d4285bbb4cd547e3e8f5a850a754d2a119717
Pasha <pasha@2vega.com>
2015-12-03 17:04:11 -0800

    Private config

---
+++ Demo/Demo/TMLPrivateConfig.h
@@ -0,0 +1,15 @@
+//
+//  TMLPrivateConfig.h
+//  Demo
+//
+//  Created by Pasha on 11/10/15.
+//  Copyright © 2015 TmlHub Inc. All rights reserved.
+//
+
+#ifndef TMLPrivateConfig_h
+#define TMLPrivateConfig_h
+
+//#define TMLApplicationKey @""
+//#define TMLAccessToken @""
+
+#endif /* TMLPrivateConfig_h */
