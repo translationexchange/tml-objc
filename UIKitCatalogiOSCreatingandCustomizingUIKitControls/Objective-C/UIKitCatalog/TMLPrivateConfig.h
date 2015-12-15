f13fb12e89ad734ef83a0df03d124048bc6da49d
Pasha <pasha@2vega.com>
2015-12-14 22:04:20 -0800

    Readd file

---
+++ UIKitCatalogiOSCreatingandCustomizingUIKitControls/Objective-C/UIKitCatalog/TMLPrivateConfig.h
@@ -0,0 +1,15 @@
+//
+//  TMLPrivateConfig.h
+//  UIKitCatalog
+//
+//  Created by Pasha on 12/14/15.
+//  Copyright © 2015 f. All rights reserved.
+//
+
+#ifndef TMLPrivateConfig_h
+#define TMLPrivateConfig_h
+
+//#define TMLAccessToken @""
+//#define TMLApplicationKey @""
+
+#endif /* TMLPrivateConfig_h */
