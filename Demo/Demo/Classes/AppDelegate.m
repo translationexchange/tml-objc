58f23fe7c17aa3ecabfada36fc81ce4e3fceb7a9
Pasha <pasha@2vega.com>
2015-12-21 22:34:29 -0800

    Move TML initialization to main
    we need to have application key available before we start decoding NIB objects

--- Demo/Demo/Classes/AppDelegate.m
+++ Demo/Demo/Classes/AppDelegate.m
@@ -9,14 +9,6 @@
 #import "AppDelegate.h"
 #import "IIViewDeckController.h"
 #import "MenuViewController.h"
-#import "TMLPrivateConfig.h"
-
-#ifndef TMLAccessToken
-#define TMLAccessToken @"8641229aae46c7d39e78657e9da0c86c80f432c21e4e4fb5bf0934673499be7a"
-#endif
-#ifndef TMLApplicationKey
-#define TMLApplicationKey @"8641229aae46c7d39e78657e9da0c86c80f432c21e4e4fb5bf0934673499be7a"
-#endif
 
 @interface AppDelegate()<TMLDelegate>
 
@@ -25,11 +17,6 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    TML *tml = [TML sharedInstanceWithApplicationKey:TMLApplicationKey accessToken:TMLAccessToken];
-    tml.delegate = self;
-    
-
     UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle: nil];
     self.window.rootViewController = [[IIViewDeckController alloc] initWithCenterViewController:[mainStoryboard instantiateViewControllerWithIdentifier: @"WelcomeViewController"]
                                                                              leftViewController:[mainStoryboard instantiateViewControllerWithIdentifier: @"MenuViewController"]];
@@ -72,13 +59,5 @@
     [alert show];
 }
 
-#pragma mark - TMLDelegate
-#if (TARGET_IPHONE_SIMULATOR)
-- (UIGestureRecognizer *)gestureRecognizerForTranslationActivation {
-    UILongPressGestureRecognizer *gestureRecognizer = [[UILongPressGestureRecognizer alloc] init];
-    gestureRecognizer.numberOfTouchesRequired = 2;
-    return gestureRecognizer;
-}
-#endif
 @end
 
--- Demo/Demo/main.m
+++ Demo/Demo/main.m
@@ -10,9 +10,19 @@
 
 #import "AppDelegate.h"
 
+#import "TMLPrivateConfig.h"
+
+#ifndef TMLAccessToken
+#define TMLAccessToken @"8641229aae46c7d39e78657e9da0c86c80f432c21e4e4fb5bf0934673499be7a"
+#endif
+#ifndef TMLApplicationKey
+#define TMLApplicationKey @"8641229aae46c7d39e78657e9da0c86c80f432c21e4e4fb5bf0934673499be7a"
+#endif
+
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+        [TML sharedInstanceWithApplicationKey:TMLApplicationKey accessToken:TMLAccessToken];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
