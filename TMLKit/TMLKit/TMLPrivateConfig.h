e04be3175be04ba74c9d528a4cc9481cc13ceb54
Pasha <pasha@2vega.com>
2016-01-20 18:27:50 -0800

    Add TMLPrivateConfig.h to TMLKit

---
+++ TMLKit/TMLKit/TMLPrivateConfig.h
@@ -0,0 +1,13 @@
+//
+//  TMLPrivateConfig.h
+//  TMLKit
+//
+//  Created by Pasha on 1/20/16.
+//  Copyright © 2016 Translation Exchange. All rights reserved.
+//
+
+#ifndef TMLPrivateConfig_h
+#define TMLPrivateConfig_h
+
+
+#endif /* TMLPrivateConfig_h */
