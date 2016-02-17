a2aadc32eacf858831953e96be0468e528ae4736
Pasha <pasha@2vega.com>
2016-02-16 22:03:06 -0800

    we shouldn't be defining these at all in private config

--- Demo/Demo/TMLPrivateConfig.h
+++ Demo/Demo/TMLPrivateConfig.h
@@ -1,6 +1,4 @@
 /* GENERATED FILE - MODIFICATIONS WILL BE LOST */
 #ifndef TMLPrivateConfig_h
 #define TMLPrivateConfig_h
-#define TMLDefaultAccessToken @""
-#define TMLDefaultApplicationKey @""
 #endif /* TMLPrivateConfig_h */
--- TMLKit/TMLKit/TMLPrivateConfig.h
+++ TMLKit/TMLKit/TMLPrivateConfig.h
@@ -1,6 +1,4 @@
 /* GENERATED FILE - MODIFICATIONS WILL BE LOST */
 #ifndef TMLPrivateConfig_h
 #define TMLPrivateConfig_h
-#define TMLServiceHost @""
-#define TMLTranslationCenterHost @""
 #endif /* TMLPrivateConfig_h */
