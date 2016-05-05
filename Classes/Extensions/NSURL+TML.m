8c0cb13f8c7d0e5df029aac896761e85e8899f41
Pasha <pasha@2vega.com>
2016-05-04 20:18:53 -0700

    Cleaner joining of query params

--- Classes/Extensions/NSURL+TML.m
+++ Classes/Extensions/NSURL+TML.m
@@ -12,16 +12,21 @@
 
 - (NSURL *)URLByAppendingQueryParameters:(NSDictionary *)queryParameters {
     NSMutableString *string = [[self absoluteString] mutableCopy];
-    if (self.query.length == 0) {
+    NSString *query = self.query;
+    NSString *joiner = @"&";
+    if (query.length == 0) {
         if ([string hasSuffix:@"?"] == NO) {
             [string appendString:@"?"];
         }
+        joiner = @"";
     }
+    
     NSCharacterSet *charSet = [NSCharacterSet URLQueryAllowedCharacterSet];
     for (NSString *key in queryParameters) {
         NSString *queryKey = [key stringByAddingPercentEncodingWithAllowedCharacters:charSet];
         NSString *queryValue = [queryParameters[key] stringByAddingPercentEncodingWithAllowedCharacters:charSet];
-        [string appendFormat:@"&%@=%@", queryKey, queryValue];
+        [string appendFormat:@"%@%@=%@", joiner, queryKey, queryValue];
+        joiner = @"&";
     }
     return [NSURL URLWithString:[string copy]];
 }
